09fcd3184d1b24e490f47697303ed6b839b7ff36
Luana Lessa <lessalsn@gmail.com>
2021-06-28 05:23:50 -0300

    Using events from api

--- src/pages/User/EventsAvailable/index.jsx
+++ src/pages/User/EventsAvailable/index.jsx
@@ -1,17 +1,20 @@
+import React, {useState, useEffect} from 'react'
 import Header from "../../../components/Header";
-import EventCard from "../../../components/EventCard";
+import EventList from "../../../components/EventList";
+
+import { api } from "../../../services/api.js";
 
 import { Container } from "./styles";
 // Change EventCard to EventList Component
 
 function EventsList() {
+ 
+
   return (
     <>
       <Header type="user" />
       <Container> 
-      <EventCard btn1 = {"Details"} btn2 = {"Subscribe"}></EventCard>
-      <EventCard btn1 = {"Details"} btn2 = {"Subscribe"}></EventCard>
-      <EventCard btn1 = {"Details"} btn2 = {"Subscribe"}></EventCard>
+      <EventList></EventList>
       </Container>
     </>
   );
--- src/providers/EventProvider.jsx
+++ src/providers/EventProvider.jsx
@@ -1,9 +1,42 @@
-import React, { createContext } from "react";
+import React, { useState, createContext, useCallback, useEffect } from "react";
 // import { useSnackbar } from "notistack";
-// import { api } from "../services/api";
+import { api } from "../services/api";
 
 export const EventContext = createContext({});
 
 export const EventProvider = ({ children }) => {
-  return <EventContext.Provider>{children}</EventContext.Provider>;
+  const [events, setEvents] = useState();
+  const [eventId, setEventId] = useState();
+
+  useEffect(async () => {
+    console.log("ok");
+    try {
+      const { data } = await api.get("/events");
+      setEvents(data);
+      console.log(events);
+    } catch (err) {
+      return null;
+    }
+  }, []);
+
+  const Subscribe = useCallback(
+    async ({ eventId }) => {
+      const { data } = await api.post("/users", {
+        eventId
+      });
+      // enqueueSnackbar("User successfully registered", {
+      //   variant: "success",
+      // });
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  );
+
+  return (
+    <EventContext.Provider
+      value={{ events, setEvents, eventId, setEventId }}
+    >
+      {children}
+    </EventContext.Provider>
+  );
 };
