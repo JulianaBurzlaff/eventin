c32c30e6c92f976330004ef8b4cc7eafbe1c3894
Luana Lessa <lessalsn@gmail.com>
2021-06-28 05:38:25 -0300

    fix event

--- src/pages/User/EventsAvailable/index.jsx
+++ src/pages/User/EventsAvailable/index.jsx
@@ -1,13 +1,8 @@
 import React, {useState, useEffect} from 'react'
 import Header from "../../../components/Header";
-<<<<<<< HEAD
 import EventList from "../../../components/EventList";
 
 import { api } from "../../../services/api.js";
-=======
-import EventCard from "../../../components/EventCard";
-import UserLayout from "../../../components/UserLayout";
->>>>>>> ea053f7f66e937c58922be2a81576d0c2a195f55
 
 import { Container } from "./styles";
 // Change EventCard to EventList Component
@@ -18,15 +13,8 @@ function EventsList() {
   return (
     <UserLayout>
       <Header type="user" />
-<<<<<<< HEAD
       <Container> 
       <EventList></EventList>
-=======
-      <Container>
-        <EventCard btn1={"Details"} btn2={"Subscribe"}></EventCard>
-        <EventCard btn1={"Details"} btn2={"Subscribe"}></EventCard>
-        <EventCard btn1={"Details"} btn2={"Subscribe"}></EventCard>
->>>>>>> ea053f7f66e937c58922be2a81576d0c2a195f55
       </Container>
     </UserLayout>
   );
