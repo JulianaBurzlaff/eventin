db94df8b6e66d2899e8ca34c5b4d311403e3bc30
Luana Lessa <lessalsn@gmail.com>
2021-06-30 03:33:14 -0300

    bug fixes

--- src/components/EventDescription/index.jsx
+++ src/components/EventDescription/index.jsx
@@ -11,18 +11,24 @@ import pin from "../../assets/pin.svg";
 
 import { AuthContext } from "../../providers/AuthProvider";
 import { useEvents } from "../../Hooks/useEvents";
+import { useUsers } from "../../Hooks/useUsers";
+
 
 export default function EventDescription(props) {
-  const { events, eventId } = useEvents();
+  const { events, eventId,generateTicket } = useEvents();
   const { user } = useContext(AuthContext);
   const [disabled, setDisabled] = useState();
   const [currentEvent, setCurrent] = useState();
   const navigate = useHistory();
 
-  console.log(events[eventId - 1].eventName)
+  const { setUserEvent } = useUsers();
+  const userId = user.id;
+
+
+  console.log(events[eventId - 1].eventName);
   console.log(events);
   useEffect(() => {
-    if ((user.events).includes(`${eventId}`)) {
+    if (user.events.includes(`${eventId}`)) {
       setDisabled(true);
     }
   }, []);
@@ -31,31 +37,45 @@ export default function EventDescription(props) {
   // setCurrent(current);
   // console.log("aaa",events);
 
+  function handleSubmit(id) {
+    setUserEvent(id, user.username);
+    setDisabled(true);
+    generateTicket({ eventId: id, userId: user.id });
+    console.log("Ok");
+  }
+
   return (
     <Container>
-      <img className="eventPic" src = {events[eventId - 1].image}></img>
-          <EventInfor>
-            <h3>{events[eventId - 1].eventName}</h3>
-            <ul>
-              <li>
-                <img src={calendar}></img>
-                <span>{events[eventId - 1].date}</span>
-              </li>
-              <li>
-                <img src={clock}></img>
-                {events[eventId - 1].time}
-              </li>
-              <li>
-                <img src={pin}></img>
-                {events[eventId - 1].location}
-              </li>
-            </ul>
-            <legend>{events[eventId - 1].description}</legend>
-            <Button onClick={() => navigate.push(`/user/events-available`)}>
-              {props.btn1}
-            </Button>
-            <Button disabled={disabled}>{props.btn2}</Button>
-          </EventInfor>
+      <img className="eventPic" src={events[eventId - 1].image}></img>
+      <EventInfor>
+        <h3>{events[eventId - 1].eventName}</h3>
+        <ul>
+          <li>
+            <img src={calendar}></img>
+            <span>{events[eventId - 1].date}</span>
+          </li>
+          <li>
+            <img src={clock}></img>
+            {events[eventId - 1].time}
+          </li>
+          <li>
+            <img src={pin}></img>
+            {events[eventId - 1].location}
+          </li>
+        </ul>
+        <legend>{events[eventId - 1].description}</legend>
+        <Button onClick={() => navigate.push(`/user/events-available`)}>
+          {props.btn1}
+        </Button>
+        <Button
+          onClick={() => {
+            handleSubmit(eventId);
+          }}
+          disabled={disabled}
+        >
+          {props.btn2}
+        </Button>
+      </EventInfor>
     </Container>
   );
 }
